86aa13eef6dfd1e6ca0fc8f1ab0fa38e6390492e
Vladimir Parfionov <jobstest18@gmail.com>
2023-03-29 18:24:18 +0400

    add auth test

---
+++ src/test/java/com/tricentis/demowebshop/tests/BaseTests.java
@@ -0,0 +1,37 @@
+package com.tricentis.demowebshop.tests;
+
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.WebDriverRunner;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import com.tricentis.demowebshop.helpers.Attach;
+import io.qameta.allure.selenide.AllureSelenide;
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+public class BaseTests {
+
+    @BeforeAll
+    static void setUp() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+
+        Configuration.baseUrl = "https://demowebshop.tricentis.com/";
+        RestAssured.baseURI = "https://demowebshop.tricentis.com/";
+        Configuration.browserSize = "1920x1080";
+        //Configuration.remote = "http://192.168.0.90:4444/wd/hub";
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("enableVNC", true);
+        capabilities.setCapability("enableVideo", true);
+        Configuration.browserCapabilities = capabilities;
+    }
+
+    @AfterEach
+    void addAttchments() {
+        Attach.screenshotAs("Last screenshot");
+        Attach.pageSource();
+        Attach.browserConsoleLogs();
+        //Attach.addVideo();
+        WebDriverRunner.closeWindow();
+    }
+}
